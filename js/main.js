713f1172378c826c199d39487a88061ef1742bef
Otto45 <austino45@gmail.com>
2014-11-21 17:24:57 +0100

    Moved scripts to main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 var inventory;
 
+function displayItems(item){
+    document.getElementById("test").innerHTML = item;
+}
+function customerLogin(){
+    var userName = document.getElementById("username").value;
+    var passWord = document.getElementById("pwd").value;
+    if(userName == 'username' && passWord == 'password'){
+        var userID = document.createTextNode("Customer: <> ID Number: <>");
+        var userTabAmount = document.createTextNode("Current Tab: <>");
+        var infoToReplace = document.getElementById("loginBlock");
+        infoToReplace.innerHTML = '';
+        infoToReplace.appendChild(userID);
+        infoToReplace.appendChild(userTabAmount);
+
+    } else {
+        alert("Incorrect username or password");
+    }
+}
+
 function init() {
     inventory = new Inventory();
     inventory.refresh();
